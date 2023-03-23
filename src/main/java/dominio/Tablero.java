299b11473376b7c0d0047ef9ff47bb3cd7b9e85b
jkort <jortealv@myuax.com>
2023-03-23 20:16:37 +0100

    MetodosContarVecinosYTransitarEstadoSiguiente

--- src/main/java/dominio/Tablero.java
+++ src/main/java/dominio/Tablero.java
@@ -28,5 +28,45 @@ public class Tablero {
         }
 
     }
+    public void generarEstadoActualPorMontecarlo() {
+        estadoActual = new int[DIMENSION][DIMENSION];
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                estadoActual[i][j] = (Math.random() < 0.5) ? 0 : 1;
+            }
+        }
+    }
+
+    public void transitarAlEstadoSiguiente() {
+    for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                int vecinosVivos = contarVecinosVivos(i, j);
+                if (vecinosVivos == 3) {
+                    estadoSiguiente[i][j] = 1;
+                } else if (vecinosVivos == 2) {
+                    estadoSiguiente[i][j] = estadoActual[i][j];
+                } else {
+                    estadoSiguiente[i][j] = 0;
+                }
+            }
+        }
+        estadoActual = estadoSiguiente;
+        estadoSiguiente = new int[DIMENSION][DIMENSION];
+    }
+
+    private int contarVecinosVivos(int i, int j) {
+        int vecinosVivos = 0;
+        for (int k = i - 1; k <= i + 1; k++) {
+            for (int l = j - 1; l <= j + 1; l++) {
+                if (k >= 0 && k < DIMENSION && l >= 0 && l < DIMENSION && !(k == i && l == j)) {
+                    vecinosVivos += estadoActual[k][l];
+                }
+            }
+        }
+        return vecinosVivos;
+    }
+
+    public String toString() {
+
 
 }
