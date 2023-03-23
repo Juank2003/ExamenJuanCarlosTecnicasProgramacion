656e5554e8c12847048be73a86180330f2dcdfd9
jkort <jortealv@myuax.com>
2023-03-23 20:07:28 +0100

    MetodoleerEstadoActual

--- src/main/java/dominio/Tablero.java
+++ src/main/java/dominio/Tablero.java
@@ -1,17 +1,31 @@
 package dominio;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Tablero {
     private static final int DIMENSION = 30;
     private int [][] estadoActual;
+    private int[][] estadoSiguiente = new int[DIMENSION][DIMENSION];
 
 
-    public void leerEstadoActual(){
+    public void leerEstadoActual() {
 
-    }
-    public void transitarAlEstadoSiguiente(){
-
-    }
-    public void generarEstadoActualPorMontecarlo(){
+        estadoActual = new int[DIMENSION][DIMENSION];
+        try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/estadoActual.txt"))) {
+            String linea;
+            int i = 0;
+            while ((linea = br.readLine()) != null) {
+                String[] valores = linea.split(" ");
+                for (int j = 0; j < valores.length; j++) {
+                    estadoActual[i][j] = Integer.parseInt(valores[j]);
+                }
+                i++;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
