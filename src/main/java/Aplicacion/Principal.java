423c477dde174b3e872eb7ae05b2e378f93bb8a5
jkort <jortealv@myuax.com>
2023-03-23 19:44:34 +0100

    PriebaPractica1

--- src/main/java/Aplicacion/Principal.java
+++ src/main/java/Aplicacion/Principal.java
@@ -1,4 +1,9 @@
 package Aplicacion;
 
-public class Principal {
+import Mates.Matematicas;
+public class Principal{
+    public static void main(String[] args){
+        System.out.println("El número PI es " + Matematicas.
+                generarNumeroPi(Integer.parseInt(args[0])));
+    }
 }
--- src/main/java/Mates/Matematicas.java
+++ src/main/java/Mates/Matematicas.java
@@ -1,8 +1,23 @@
 package Mates;
-import Mates.Matematicas;
+
+import java.util.Random;
 
 public class Matematicas {
-    public static void generarNumeroPi(long pasos){
+    public static double generarNumeroPi(long pasos){
         double aciertos = 0;
+        double radio = 1;
+        double ladoCuadrado = 2 * radio;
+        Random rnd = new Random();
+
+        for (long i = 0; i < pasos; i++) {
+            double x = rnd.nextDouble() * ladoCuadrado - radio;
+            double y = rnd.nextDouble() * ladoCuadrado - radio;
+            double distancia = Math.sqrt(x * x + y * y);
+            if (distancia <= radio) {
+                aciertos++;
+            }
+        }
+
+        return 4 * (aciertos / pasos);
     }
 }
